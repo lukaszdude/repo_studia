215eccc2cdc2012aa0dee5612ab40c39e7a3b5f1
Łukasz Duda <your_email_address@example.com>
2022-04-01 17:06:02 +0200

    schemat bazy danych i funkcje CRUD

--- paw-koncerty/functions/paw-zadanie.js
+++ paw-koncerty/functions/paw-zadanie.js
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDoc, getFirestore, setDoc}  from "firebase/firestore";
+import { collection, addDoc, doc, deleteDoc, getDocs, query, where, updateDoc, deleteField } from "firebase/firestore"; 
 
-// Your web app's Firebase configuration
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
   apiKey: "AIzaSyDfK-gbvNifIYK_xUJn-MFQuVVGAevxBVo",
   authDomain: "paw-concerts.firebaseapp.com",
@@ -16,6 +15,78 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-//const db = getFirestore();
 const app = initializeApp(firebaseConfig);
-console.log("hello world");
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+//Dodanie nowej kolekcji a w niej parę dokumentów
+try {
+  const docRef = collection(db, "cities");
+  await setDoc(doc(docRef, "Warszawa"), {
+    country: "Poland",
+    population: 1800000,
+    capital: true,
+    Rivers: ["Wisła", "Wilanówka", "Długa"] });
+  await setDoc(doc(docRef, "Wrocław"), {
+    country: "Poland",
+    population: 640000,
+    capital: false,
+    Rivers: ["Odra", "Bystrzyca", "Oława", "Ślęza"] });
+  await setDoc(doc(docRef, "Berlin"), {
+    country: "Germany",
+    population: 3700000,
+    capital: true,
+    Rivers: ["Sprewa", "Hawela"] });
+  await setDoc(doc(docRef, "Mediolan"), {
+    country: "Italia",
+    population: 1400000,
+    capital: false,
+    Rivers: ["Ticino", "Adda", "Pad"] });
+
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+/*//Wyświetlenie jednego zdefiniowange dokumentu
+const docCity = doc(db, "cities", "Wrocław");
+const docSnap = await getDoc(docCity);
+if(docSnap.exists()){
+  console.log(docSnap.data());
+} else{
+  console.log("Dokument nie istnieje!");
+}*/
+
+/*
+//wyświetlanie konkretnych dokumentów z zapytaniem
+const q = query(collection(db, "cities"), where("population", "<", 1500000));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  console.log("Populacja mniejsza niż 1,5mln - ", doc.id, ":\n", doc.data());
+});
+*/
+
+/*//Zaktualizowanie dokumentu Mediolan
+const updateData = doc(db, "cities", "Mediolan");
+  updateDoc(updateData, {
+    population: 1650000
+  });*/
+
+//Usunięcie konkretnego pola z dokumentu
+  /*
+const deletePole = doc(db, "cities", "Berlin");
+await updateDoc(deletePole, {
+  population: deleteField()
+});*/
+
+
+//Usunięcie całego dokumentu
+//await deleteDoc(doc(db, "cities", "Mediolan"));
+
+/*
+console.log("----- Wyświetlenie wszystkich dokumentów -----")
+const querySnapshot = await getDocs(collection(db, "cities"));
+querySnapshot.forEach((doc) => {
+  console.log(doc.id, ":\n", doc.data());
+});*/
